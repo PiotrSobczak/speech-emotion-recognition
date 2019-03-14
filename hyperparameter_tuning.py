daf240e3592299e5ef5e3dd1ba6a9f53e811a9ac
piosobc <piosobc@gmail.com>
2019-03-14 15:51:18 +0100

    Change reg_ratio randomization

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         params["n_layers"] = np.random.randint(1, 4)
         params["hidden_dim"] = np.random.randint(64, 1200)
         params["dropout"] = 0.1 + np.random.rand() * 0.85
-        params["reg_ratio"] = np.random.rand() * 0.00001
+        params["reg_ratio"] = np.random.rand()*(10**(np.random.randint(-4, -2)))
         params["batch_size"] = np.random.randint(16,150)
         params["seq_len"] = np.random.randint(10, 50)
         run_training(**params)
