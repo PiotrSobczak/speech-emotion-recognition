05fc24da8312adc36c40d3e189d1a9da39671ae2
piosobc <piosobc@gmail.com>
2019-04-15 19:47:48 +0200

    Add first model with self-attention

--- models.py
+++ models.py
@@ -1,4 +1,54 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def init_linear(m):
+    if type(m) == nn.Linear:
+        torch.nn.init.xavier_normal(m.weight)
+        nn.init.constant(m.bias, 0)
+
+
+class AttnDecoderRNN(nn.Module):
+    """ Based on: https://medium.com/apache-mxnet/sentiment-analysis-via-self-attention-with-mxnet-gluon-dc774d38ba69 
+    Also take a look at : https://github.com/gogyzzz/localatt_emorecog/blob/master/localatt/localatt.py"""
+    def __init__(self, cfg):
+        super(AttnDecoderRNN, self).__init__()
+        self.do2 = nn.Dropout()
+
+        self.blstm = torch.nn.LSTM(cfg.emb_dim, cfg.hidden_dim, num_layers=cfg.n_layers, bidirectional=cfg.bidirectional)
+
+        self.d_dim = 64
+        self.r_dim = 32
+
+        self.fc1 = nn.Linear(cfg.hidden_dim, self.d_dim)
+        self.fc2 = nn.Linear(self.d_dim, self.r_dim)
+        self.fc3 = nn.Linear(self.r_dim, cfg.num_classes)
+
+        self.apply(init_linear)
+
+    def forward(self, input):
+        """ input = [seq_len, batch size, emb_dim] """
+        input = torch.Tensor(input)
+
+        """ output = [seq_len, batch size, hidden_dim] """
+        output, (hidden, cell) = self.blstm(input)
+
+        """ output_trans = [seq_len, batch size, d_dim] """
+        output_trans = self.fc1(output)
+        output_trans = F.tanh(output_trans)
+
+        """ output_scores = [seq_len, batch size, r_dim] """
+        output_scores = self.fc2(output_trans)
+        attention = F.softmax(output_scores)
+
+        """ output_trans2_sum = [batch size, r_dim] """
+        outputs_scaled = torch.sum(attention, dim=0)
+
+        """ output_logits = [batch size, num_classes] """
+        output_logits = self.fc3(outputs_scaled)
+
+        return F.softmax(output_logits)
 
 
 class RNN(torch.nn.Module):
