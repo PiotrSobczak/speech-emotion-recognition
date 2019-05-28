ce1718d2a7fead8451e3a77ae5c11fd1492e657c
piosobc <piosobc@gmail.com>
2019-05-28 02:00:55 +0200

    Remove relu before softmax

--- models.py
+++ models.py
@@ -203,5 +203,5 @@ class CNN(nn.Module):
         x = x.view(-1, self.flat_size)
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
-        x = F.relu(self.fc2(x))
+        x = self.fc2(x)
         return x
