6e580d2e4e2f84a6d3f8879d4e94a3f1a3a3e20a
piosobc <piosobc@gmail.com>
2019-06-19 13:13:39 +0200

    Change tensor type dynamically

--- batch_iterator.py
+++ batch_iterator.py
@@ -2,13 +2,14 @@ import torch
 import numpy as np
 from math import ceil
 
+
 class BatchIterator:
-    def __init__(self, dataset, labels, batch_size=50, embedding_size=400):
+    def __init__(self, dataset, labels, batch_size=50):
         self._dataset = dataset
         self._size = len(dataset)
         self._labels = labels
         self._batch_size = batch_size
-        self._embedding_size = embedding_size
+        self._tensor_type = torch.cuda.LongTensor if torch.cuda.is_available() else torch.LongTensor
 
     def __len__(self):
         return int(self._size/self._batch_size)
@@ -18,10 +19,10 @@ class BatchIterator:
         for i in range(num_batches):
             input_batch = self._dataset[i*self._batch_size:(i+1)*self._batch_size]
             labels_batch = self._labels[i*self._batch_size:(i+1)*self._batch_size]
-            yield input_batch, torch.cuda.LongTensor(labels_batch)
+            yield input_batch, self._tensor_type(labels_batch)
 
     def shuffle(self):
         new_order = np.random.permutation(self._size)
         self._dataset = self._dataset[new_order]
         self._labels = self._labels[new_order]
-        
+
